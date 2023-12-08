2c82e7504b581c0d1056831c6824f92ded3b1c05
Michal Jazwinski <mail@mihow.pl>
2023-12-08 17:49:50 +0100

    Add project files.

---
+++ WpfApp11/AppTextBox.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Interaction logic for AppTextBox.xaml
+    /// </summary>
+    public partial class AppTextBox : TextBox
+    {
+        public static new readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(AppTextBox), new FrameworkPropertyMetadata(new PropertyChangedCallback(OnTextPropertyChanged)));
+        public new string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        private static void OnTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Debug.WriteLine("OnTextPropertyChanged");
+        }
+
+        public AppTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp11/MainWindowVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp11
+{
+    internal class MainWindowVM : INotifyPropertyChanged
+    {
+        private string _testString = "Better apologize Clemens";
+
+        public string TestString
+        {
+            get { return _testString; }
+            set { _testString = value; NotifyPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
